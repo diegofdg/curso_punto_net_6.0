44d1c1cf82dc124417118cbad394fc861ae61dce
diegofdg <diegofdg@gmail.com>
2022-05-25 20:28:58 -0300

    09_clase_24-05-2022

---
+++ 09_EjercicioMatrices2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Ingrese cantidad de numeros primos deseados:");
+int n = int.Parse(Console.ReadLine());
+
+int[] primos = new int[n];
+// List<int> primos2 = new List<int>();
+
+int posicion = 0;
+int numero = 1;
+
+while (posicion < n) {
+    int aux = 0;
+    for (int i = 1; i <= numero; i++) {
+        if (numero % i == 0) {
+            aux++;
+        }
+    }
+    if (aux == 2) {
+        primos[posicion] = numero;
+        posicion++;
+    }
+    numero++;
+}
+foreach (int i in primos) {
+    Console.WriteLine(i);
+}
+Console.WriteLine("Pulse una tecla para finalizar");
+Console.ReadKey();
