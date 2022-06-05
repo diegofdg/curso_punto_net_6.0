6162cf396f3f3a8cbb0837b419ab1d7fd1403f70
diegofdg <diegofdg@gmail.com>
2022-06-05 09:14:05 -0300

    11_clase_31-05-2022

---
+++ 11_IntroObjetos/Perro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_IntroObjetos {
+    internal class Perro {
+        // Campos
+        public string Raza;
+
+        public string Tamanio;
+
+        public int Edad;
+
+        public string Color;
+
+        public void Ladrar() {
+            Console.WriteLine("Guauu");
+        }
+    }
+}
