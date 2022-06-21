c41825804bcfe1011c79d7a77015e657048a9075
diegofdg <diegofdg@gmail.com>
2022-06-21 19:16:15 -0300

    13_clase_07-06-2022

---
+++ 13_Encapsulamiento/Program.cs
@@ -0,0 +1,15 @@
+﻿using _13_Encapsulamiento.Modelo;
+
+namespace _13_Encapsulamiento {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Estamos creando un nuevo Auto. Ingrese Tipo de Motor:");
+            var tipoMotor = Console.ReadLine();
+
+            var miAuto = new Auto();
+            miAuto.TipoDeMotor = tipoMotor;
+
+            Console.WriteLine(miAuto.TipoDeMotor);
+        }
+    }
+}
