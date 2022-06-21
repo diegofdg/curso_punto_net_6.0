cfe5ebec3b161053ccaa11adfd383bcb2cc98079
diegofdg <diegofdg@gmail.com>
2022-06-21 19:11:41 -0300

    13_clase_07-06-2022

---
+++ 13_Constructores/Modelo/Ocupacion.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Constructores.Modelo {
+    internal class Ocupacion {
+        public string Descripcion { get; set; }
+    }
+}
