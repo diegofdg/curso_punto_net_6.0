a9d9f9008e0f4643d84f2af58ec8e019b1f18a73
diegofdg <diegofdg@gmail.com>
2022-06-29 20:05:30 -0300

    16_clase_16-06-2022

---
+++ 16_GestorPersonas/Program.cs
@@ -0,0 +1,19 @@
+﻿using _16_GestorPersonas.Modelo;
+using _16_GestorPersonas.Repositorio;
+
+//Gestor de personas trabajadoras (empleados, desempleados, jubilados)
+//Insertar personas en una base de datos, mostrar datos de una determinada persona o
+//de todas las personas.
+
+var repositorio = new RepositorioDePersonas();
+
+//Le pido al usuario los datos de una persona para agregar
+//El usuario debería elegir el tipo de persona a agregar
+
+var usuarioNombre = "Ezequiel";
+var usuarioApellido = "Etchecoin";
+
+repositorio.Insertar(new Desempleado {
+    Nombre = usuarioNombre,
+    Apellido = usuarioApellido
+});
---
+++ 16_GestorPersonas/Repositorio/RepositorioDePersonas.cs
@@ -0,0 +1,41 @@
+﻿using _16_GestorPersonas.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_GestorPersonas.Repositorio {
+    public class RepositorioDePersonas {
+        public List<Persona> Personas { get; set; }
+
+        public RepositorioDePersonas() {
+            Personas = new List<Persona>();
+        }
+
+        public void Insertar(Persona persona) {
+            Personas.Add(persona);
+        }
+        public void Eliminar(string numeroDocumento) {
+            //Definir como eliminar una persona de la Lista de Personas.
+        }
+
+        public void Actualizar(Persona persona) {
+            foreach (var personaActual in Personas) {
+                if (personaActual.NumeroDeDocumento == persona.NumeroDeDocumento) {
+                    personaActual.Nombre = persona.Nombre;
+                    personaActual.Apellido = persona.Apellido;
+                    personaActual.FechaNacimiento = persona.FechaNacimiento;
+                }
+            }
+        }
+        public void Actualizar(string numeroDocumento, string nombre, string apellido) {
+            foreach (var personaActual in Personas) {
+                if (personaActual.NumeroDeDocumento == numeroDocumento) {
+                    personaActual.Nombre = nombre;
+                    personaActual.Apellido = apellido;
+                }
+            }
+        }
+    }
+}
