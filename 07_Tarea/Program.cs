58bfa839cd36dbabc285d94a69ae28123a7c394c
diegofdg <diegofdg@gmail.com>
2022-07-01 16:08:44 -0300

    07_tarea_solucion_propuesta_en_clase_27-05-22

--- 07_Tarea/Program.cs
+++ 07_Tarea/Program.cs
@@ -1,54 +1,42 @@
-﻿/*
-Clase 7: Tarea - Escribe un programa que:
-1) Pida 10 números al usuario.
-2) Obtener la suma de todos los números ingresados.
-3) Obtener cual es el mayor de todos los números.
-4) Obtener cual es el menor de todos los números.
-5) Obtener el promedio de todos los números.
-6) Mostrar todos los números ingresados por pantalla.
-7) Mostrar los valores calculados en los puntos 2, 3, 4 y 5.
+﻿Console.Clear();
 
-Recuerden que no nos deben adjuntar archivos comprimidos ni imagenes.
-Pueden adjuntarnos el archivo .cs, o el codigo como texto. 
- */
+int[] numeros = new int[10];
 
-float[] numeros = new float[10];
-float suma = 0;
-
-for (int i = 0; i < numeros.Length; i++) {
-    Console.WriteLine($"Por favor ingrese el número: {i + 1}");
-    numeros[i] = int.Parse(Console.ReadLine());
-    suma += numeros[i];
+// Pedimos al usuario que ingrese 10 números
+for (int i = 0; i < 10; i++) {
+    Console.WriteLine($"Ingrese el número: {i + 1}");
+    numeros[i] = int.Parse(Console.ReadLine());    
 }
 
-Console.Clear();
+int suma = 0;
 
-float minimo = numeros[0];
-float maximo = numeros[0];
-
-for (int i = 0; i < numeros.Length; i++) {
-    if(numeros[i] < minimo) {
-        minimo = numeros[i];
-    } else if (numeros[i] > maximo) {
-        maximo = numeros[i];
-    }    
+for (int i = 0; i < 10; i++) {
+    suma = suma + numeros[i];
 }
 
-float promedio = suma / numeros.Length;
-
-
-Console.Write("Los números ingresados son: ");
-for (int i = 0; i < numeros.Length; i++) {
-    if (i == numeros.Length - 1) {
-        Console.WriteLine(numeros[i]);
-    } else {
-        Console.Write($"{numeros[i]}, ");
+int mayor = numeros[0];
+for (int i = 0; i < 10; i++) {
+    if (numeros[i] > mayor) {
+        mayor = numeros[i];
     }
 }
 
-Console.WriteLine($"La suma de todos los valores ingresados es: {minimo}");
-Console.WriteLine($"El mínimo valor ingresado es: {minimo}");
-Console.WriteLine($"El máximo valor ingresado es: {maximo}");
-Console.WriteLine($"El promedio de los valores ingresados es: {promedio}");
+int menor = numeros[0];
+for (int i = 0; i < 10; i++) {
+    if (numeros[i] < menor) {
+        menor = numeros[i];
+    }
+}
 
-Console.WriteLine("\nPrograma finalizado correctamente");
+double promedio = suma / 10d;
+
+Console.WriteLine("Los números ingresados son: ");
+for (int i = 0; i < 10; i++) {
+    Console.Write(numeros[i] + " ");
+}
+
+Console.WriteLine("");
+Console.WriteLine($"La suma es: {suma}");
+Console.WriteLine($"El menor es: {menor}");
+Console.WriteLine($"El mayor es: {mayor}");
+Console.WriteLine($"El promedio es: {promedio}");
