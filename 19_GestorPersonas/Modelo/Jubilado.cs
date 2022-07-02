a435c4f5b67e3b21651a57fa5fd2b6294dec84d3
diegofdg <diegofdg@gmail.com>
2022-07-02 19:16:01 -0300

    19_clase_28-06-2022

---
+++ 19_GestorPersonas/Modelo/Jubilado.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_GestorPersonas.Modelo {
+    public class Jubilado : Persona {
+        public int AniosDeAporte { get; set; }
+        public char Categoria { get; set; }
+    }
+}
---
+++ 19_GestorPersonas/Repositorio/RepositorioDePersonas.cs
@@ -0,0 +1,59 @@
+﻿using _19_GestorPersonas.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_GestorPersonas.Repositorio {
+    public class RepositorioDePersonas {
+        //Usamos un diccionario para guardar las personas.
+        public Dictionary<string, Persona> Personas { get; set; }
+
+        public RepositorioDePersonas() {
+            Personas = new Dictionary<string, Persona>();
+        }
+
+        public void Insertar(Persona persona) {
+            var numeroDocumento = persona.NumeroDeDocumento;
+            var personaExiste = Personas.ContainsKey(numeroDocumento);
+            if(!personaExiste) {
+                Personas[numeroDocumento] = persona;
+            }
+        }
+
+        public void Eliminar(string numeroDocumento) {
+            Personas[numeroDocumento] = null;            
+        }
+
+        public void Actualizar(Persona persona) {
+            var personaAActualizar = Personas[persona.NumeroDeDocumento];
+
+            if(personaAActualizar != null) {
+                personaAActualizar.Nombre = persona.Nombre;
+                personaAActualizar.Apellido = persona.Apellido;
+                personaAActualizar.FechaNacimiento = persona.FechaNacimiento;
+            }
+        }
+
+        public void Actualizar(string numeroDocumento, string nombre, string apellido) {
+            var personaAActualizar = Personas[numeroDocumento];
+            if (personaAActualizar != null) {                
+                    personaAActualizar.Nombre = nombre;
+                    personaAActualizar.Apellido = apellido;                
+            }
+        }
+
+        public bool Existe(string numeroDeDocumento)
+        {
+            return Personas.ContainsKey(numeroDeDocumento);
+        }
+
+        public bool Existe(Persona persona)
+        {
+            return Existe(persona.NumeroDeDocumento);
+        }
+
+        
+    }
+}
