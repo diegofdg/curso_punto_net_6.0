b34eded1e380f0d02a62e30da31da09217cd5b77
diegofdg <diegofdg@gmail.com>
2022-07-03 08:27:58 -0300

    20_clase_30-06-2022

---
+++ 20_ClasesParcialesYAnonimas/Clases/MisExtensiones.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_ClasesParcialesYAnonimas.Clases {
+    public static class MisExtensiones {
+        public static int CalcularCredito(this Proveedor p) {
+            if(p.Nombre == "Juan") {
+                return 100;
+            }
+            return 0;
+        }
+
+        public static int ContarPalabras(this String s, char separador = ' ') {            
+            return s.Split(separador).Length;
+        }
+
+        // public static int ContarPalabras(this string s, string separador)
+        // {
+        //     return s.Split(separador).Length;
+        // }
+    }
+}
---
+++ 20_ClasesParcialesYAnonimas/Clases/Proveedor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_ClasesParcialesYAnonimas.Clases
+{
+    public class Proveedor : Persona
+    {
+        public Proveedor(string nombre, string apellido) : base(nombre, apellido) {
+
+        }
+    }
+}
