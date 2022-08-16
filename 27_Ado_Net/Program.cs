85ae98a2b1f16112989767046e0478ae68d8ba5f
diegofdg <diegofdg@gmail.com>
2022-08-16 18:44:47 -0300

    27_clase_09-08-2022

---
+++ 27_Ado_Net/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.SqlClient;
+
+//var connectionString = "Provider=MSOLEDBSQL;Server=myServerName\theInstanceName;Database=myDataBase;Trusted_Connection=yes;";
+
+var connectionString = "Server=DESKTOP-QR7T2GC;Database=Northwind;Trusted_Connection=yes;";
+
+using (SqlConnection connection = new SqlConnection(connectionString))
+{
+    connection.Open();
+
+    var command = new SqlCommand("SELECT * FROM Customers", connection);
+    var reader = command.ExecuteReader();
+    while (reader.Read())
+    {
+        Console.WriteLine($"{reader["CustomerID"]} | {reader["CompanyName"]} | {reader["ContactName"]}");
+    }
+}
