41f7f332b030c44e63ef35089ce839666f853796
diegofdg <diegofdg@gmail.com>
2022-08-26 08:34:13 -0300

    32_clase_25-08-2022

---
+++ 32_WebApplication2/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace _32_WebApplication2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
