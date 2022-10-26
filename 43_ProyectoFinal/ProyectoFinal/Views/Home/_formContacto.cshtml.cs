79610198264da33242216528a8022de34e641440
diegofdg <diegofdg@gmail.com>
2022-10-26 18:54:53 -0300

    48_clase_25-10-2022

---
+++ 43_ProyectoFinal/ProyectoFinal/Views/Home/_formContacto.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProyectoFinal.Views.Home
+{
+    public class _formContactoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
