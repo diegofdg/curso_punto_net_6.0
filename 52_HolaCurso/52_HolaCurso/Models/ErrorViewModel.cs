edfa0e945b942f3129ca832f7694bd8e74f27a7d
diegofdg <diegofdg@gmail.com>
2023-01-19 07:00:19 -0300

    53_tp_final_bingo-comenzando con el proyecto

---
+++ 52_HolaCurso/52_HolaCurso/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _52_HolaCurso.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ 53_TP_Final_Bingo/TP_Final_Bingo/TP_Final_Bingo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TP_Final_Bingo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
