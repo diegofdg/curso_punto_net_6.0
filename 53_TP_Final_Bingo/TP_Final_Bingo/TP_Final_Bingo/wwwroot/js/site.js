afb0894ede9cdd81fd9ac00eb984b086a4d262ab
diegofdg <diegofdg@gmail.com>
2023-01-22 08:25:24 -0300

    53_tp_final_bingo-terminada la lógica de la aplicación en el frontend

--- 53_TP_Final_Bingo/TP_Final_Bingo/TP_Final_Bingo/wwwroot/js/site.js
+++ 53_TP_Final_Bingo/TP_Final_Bingo/TP_Final_Bingo/wwwroot/js/site.js
@@ -1,51 +1,108 @@
-﻿console.log("Aplicación Iniciada");
-
-const btn_lanzar_bolilla = document.getElementById("btn-lanzar-bolilla");
+﻿const btn_lanzar_bolilla = document.getElementById("btn-lanzar-bolilla");
 const span_ultima_bolilla = document.getElementById("ultima-bolilla");
 const span_carton_ganador = document.getElementById("carton-ganador");
-btn_lanzar_bolilla.addEventListener("click", lanzarBolilla);
 
 let bolillero = [];
+let carton1 = guardarCarton(0, 27);
+let carton2 = guardarCarton(27, 54);
+let carton3 = guardarCarton(54, 81);
+let carton4 = guardarCarton(81, 108);
+let cartones = [...carton1, ...carton2, ...carton3, ...carton4];
+let ganadores = [];
+
+span_carton_ganador.textContent = "";
+btn_lanzar_bolilla.addEventListener("click", lanzarBolilla, true);
 
 function lanzarBolilla() {
+    const numero = generarNumero();
+
+    revisarCartones(numero, cartones);
+
+    if (revisarGanadores()) {
+        btn_lanzar_bolilla.removeEventListener("click", lanzarBolilla, true);
+        btn_lanzar_bolilla.disabled = true;
+        guardarDatosEnServidor();
+    };
+}
+
+function generarNumero() {
     let numero = 0;
     let encontreUnoNuevo = false;
 
     while (!encontreUnoNuevo) {
         numero = Math.floor(Math.random() * (91 - 1) + 1);
         encontreUnoNuevo = true;
-        if (bolillero.includes(numero)) {
-            if (bolillero.length == 90) {
-                console.log("No hay mas números");
-            } else {
-                encontreUnoNuevo = false;
-                continue;
-            }
+
+        if (bolillero.includes(numero)) {            
+            encontreUnoNuevo = false;
         } else {
             bolillero.push(numero);
             span_ultima_bolilla.textContent = numero;
-            console.log(numero);
-        }                
-    }  
+        }
+    }
+    return numero;
 }
 
-let carton1 = guardarCarton(0, 27);
-let carton2 = guardarCarton(27, 54);
-let carton3 = guardarCarton(54, 81);
-let carton4 = guardarCarton(81, 108);
+function revisarCartones(numero, carton) {
+    carton.forEach((value, index) => {
+        if (value == numero) {
+            const encontrado = document.querySelector(`[data-id="${index}"]`);
+            encontrado.classList.add("encontrado");
+            
+            if (index < 27) {
+                carton1 = carton1.filter((item) => item !== numero);
+            }
+            if (index >= 27 && index < 54) {
+                carton2 = carton2.filter((item) => item !== numero);
+            }
+            if (index >= 54 && index < 81) {
+                carton3 = carton3.filter((item) => item !== numero);
+            }
+            if (index >= 81 && index < 108) {
+                carton4 = carton4.filter((item) => item !== numero);
+            }
+        }
+    });
+}
+
+function revisarGanadores() {
+    if (carton1.length == 12) {
+        ganadores.push("1");
+    }
+
+    if (carton2.length == 12) {
+        ganadores.push("2");
+    }
+
+    if (carton3.length == 12) {
+        ganadores.push("3");
+    }
+
+    if (carton4.length == 12) {
+        ganadores.push("4");
+    }
+
+    if (ganadores.length > 0) {
+        span_carton_ganador.textContent = ganadores.toString();
+        return true;
+    } else {
+        return false;
+    }
+};
 
 function guardarCarton(desde, hasta) {        
     const carton = [];
-    for (let i = desde; i < hasta; i++) {
-        const numero = document.querySelector(`[data-id="${i}"]`).textContent;
-        if (numero !== "") {
+    for (let i=desde; i<hasta; i++) {
+        const numero = Number(document.querySelector(`[data-id="${i}"]`).textContent);
+        if (numero == "") {
+            carton.push(0);
+        } else {
             carton.push(numero);
         }
     }
     return carton;
 }
 
-//carton1.forEach(item => console.log(item));
-//carton2.forEach(item => console.log(item));
-//carton3.forEach(item => console.log(item));
-//carton4.forEach(item => console.log(item));
+function guardarDatosEnServidor() {
+    console.log("Enviando los datos al servidor");
+}
